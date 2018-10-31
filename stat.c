71c53adb63b1ee457a0590ff03e8841008a622c4
Jack Lu <jlu2025@gmail.com>
2018-10-31 03:16:38 -0400

    complete

--- stat.c
+++ stat.c
@@ -4,10 +4,12 @@
 #include <string.h>
 #include <errno.h>
 #include <time.h>
+#include <pwd.h>
+#include <grp.h>
 
-int main(int argn, char** argv)
+int main(int argc, char** argv)
 {
-    if (argn < 2)
+    if (argc < 2)
     {
         printf("Error: No filename given\n");
         return 1;
@@ -27,6 +29,49 @@ int main(int argn, char** argv)
     printf("Permissions: %o\n", s->st_mode % 01000);
     printf("Time of last access: %s", ctime(&(s->st_atime)));
 
+
+    char * str = malloc(100);
+    long int size = s->st_size;
+    printf("\nPrinting human readable format...\n");
+    sprintf( str, "size of stat.c is %ld bytes, %f KB, %f MB, %f GB", 
+            size, size/1000., size/1000000., size/1000000000. );
+    printf("%s\n", str);
+
+
+    printf("\nPrinting permissions in ls -l form...\n");
+    int p = s->st_mode % 01000;
+    // printf("IN BINARY 110100100\n");
+    // printf(" all %o\n", p);
+    // printf(" owner %o\n", p >> 6 & 7);
+    // printf(" group %o\n", p >> 3 & 7);
+    // printf(" other %o\n", p & 7);
+
+    char * rwx[] = {"---", "--x", "-w-", "-wx", "r--", "r-x", "rw-", "rwx"};
+
+    struct passwd *pw = getpwuid(s->st_uid);
+    struct group  *gr = getgrgid(s->st_gid);
+
+    char * owner = pw->pw_name;
+    char * group = gr->gr_name;
+
+    // get rid of \n at end of ctime format
+    char * oof = ctime(&(s->st_mtime));
+    for (char * ooof = oof; *ooof != '\0';ooof++){
+        if (*ooof == '\n'){
+            *ooof = '\0';
+        }
+    }
+
+    char * ll = malloc(50);
+    sprintf(ll, "%s%s%s 1 %s %s %ld %s %s\n", rwx[p >> 6 & 7], rwx[p >> 3 & 7], rwx[p & 7],
+           owner, group, size, oof, argv[1]);
+    printf("%s\n", ll);
+    
+
+
     free(s);
+    free(str);
+    free(owner);
+    free(group);
     return 0;
 }
